a465ba7f187f383b831d4a7aac6a02f04f9cc316
Nathan Gearke <n.gearke@gmail.com>
2021-04-08 23:14:12 -0500

    Went off roading and built my own edit button and method, but didn't get the UI to update yet.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const cafeList = document.querySelector('#cafe-list'); //grabs ul from html
-const form = document.querySelector('#add-cafe-form');
+const addForm = document.querySelector('#add-cafe-form');
+
+const editBtn = document.querySelector('#edit-cafe-form');
+const editForm =  document.getElementById("edit-doc");
+
+let currentId;
 
 
 
@@ -9,15 +14,19 @@ function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    const editButton = document.createElement('button')
     const deleteCross = document.createElement('div') //did div here, but span would work, this is the x for delete
 
     li.setAttribute('data-id', doc.id); //doc.id references the unique key, kinda like adding a class with a value of the unique key
     name.textContent = doc.data().name; //makes name equal to the name of doc
     city.textContent = doc.data().city; //makes city equal the city name in doc
+    editButton.textContent = "EDIT"
     deleteCross.textContent = "x"; //adds an x to the div to be our delete button
+    
 
     li.appendChild(name); //add the name to the list
     li.appendChild(city); //add the city to the list
+    li.appendChild(editButton)
     li.appendChild(deleteCross); //adds our x's the each list item
 
     cafeList.appendChild(li); //adds the whole li to the cafeList
@@ -29,6 +38,31 @@ function renderCafe(doc){
         db.collection('cafes').doc(id).delete(); //uses id to grab a single doc and delete it from firestore
 
     }) 
+
+    editButton.addEventListener('click', (e) => {
+        e.stopPropagation(); //stops bubbling and propagation
+        let id = e.target.parentElement.getAttribute('data-id'); //gets the unique id of the element that we click
+        currentId = id;
+        
+        if (editForm.style.visibility === "hidden"){
+            editForm.style.visibility = "visible"
+        } else {
+            editForm.style.visibility = "hidden"
+        }
+        
+
+        //we need to add a form that collects and allows the passing of data
+        //then we need to save the new data to the old object
+        //then update the UI
+        
+
+        // db.collection('cafes').doc(id).update({
+        //     name: 'Test name',
+        //     city: 'Test city'
+        // })
+
+    }) 
+
 }
 
 //getting data
@@ -41,18 +75,17 @@ function renderCafe(doc){
 //     })
 // })
 
-
 // saving data
-form.addEventListener('submit', (e) => {
+addForm.addEventListener('submit', (e) => {
     e.preventDefault() //prevents page refresh
     db.collection('cafes').add({
         //object to add and add it to cafes collection
-        name: form.name.value,
-        city: form.city.value
+        name: addForm.name.value,
+        city: addForm.city.value
     })
     //clears values that were typed in the form after button is clicked.
-    form.name.value = '';
-    form.city.value = '';
+    addForm.name.value = '';
+    addForm.city.value = '';
 })
 
 // real-time listener
@@ -69,4 +102,19 @@ db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
             cafeList.removeChild(li); //remove it from DOM
         }
     })
+})
+
+//edit form
+editForm.addEventListener('submit', (e) => {
+    e.preventDefault() //prevents page refresh
+    db.collection('cafes').doc(currentId).update({
+        //object to add and add it to cafes collection
+        name: editForm.name.value,
+        city: editForm.city.value
+    })
+    //clears values that were typed in the form after button is clicked.
+    editForm.name.value = '';
+    editForm.city.value = '';
+    editForm.style.visibility = "hidden";
+    
 })
